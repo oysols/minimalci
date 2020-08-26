669b15b888fdef021022d4dab6e2c6e155cd1b2a
Øystein Olsen <oystein.olsen@softwarelab.no>
2020-08-26 14:04:26 +0200

    Improve monkey patched print

--- minimalci/taskrunner.py
+++ minimalci/taskrunner.py
@@ -1,34 +1,46 @@
 import importlib.util
+import contextlib
 import inspect
 from pathlib import Path
 import time
-from typing import List, Type, Any
+from typing import List, Type, Any, Iterator
 import threading
 import datetime
 import functools
 import builtins
 import sys
+import traceback
+import argparse
+import signal
 
-from .tasks import Task, State, Status
+from .executors import global_kill_signal_handler, Stash
+from .tasks import Task, State
 
 
 def print_with_thread_prefix(global_log_write_lock: threading.Lock, logdir: Path, *args: Any, **kwargs: Any) -> None:
     # Swallows kwargs to keep signature compatible with builtins.print
     # TODO: use json-based logging instead
     prefix = threading.current_thread().name.split("-")[0]
-    raw_print_string = " ".join([str(arg) for arg in args])
-    for print_string in raw_print_string.splitlines():
-        timestamp = datetime.datetime.utcnow().isoformat()
-        line = f"{timestamp} {prefix:<20} {print_string}\n"
-        sys.stdout.write(line)
-        with global_log_write_lock:
-            with open(logdir / "output.log", "a") as f:
-                f.write(line)
+    raw_print_string = " ".join([str(arg) for arg in args]).strip()
+    if raw_print_string:
+        for print_string in raw_print_string.splitlines():
+            timestamp = datetime.datetime.utcnow().isoformat()
+            line = f"{timestamp} {prefix:<20} {print_string}\n"
+            sys.stdout.write(line)
+            with global_log_write_lock:
+                with open(logdir / "output.log", "a") as f:
+                    f.write(line)
 
 
-def monkey_patch_print(logdir: Path) -> None:
+@contextlib.contextmanager
+def monkey_patch_print(logdir: Path) -> Iterator[None]:
+    original_print = builtins.print
     global_log_write_lock = threading.Lock()
     builtins.print = functools.partial(print_with_thread_prefix, global_log_write_lock, logdir)
+    try:
+        yield
+    finally:
+        builtins.print = original_print
 
 
 def get_tasks_from_module(obj: object) -> List[Type[Task]]:
@@ -42,56 +54,42 @@ def get_tasks_from_module(obj: object) -> List[Type[Task]]:
 
 
 def run_tasks(task_classes: List[Type[Task]], state: State) -> None:
-    monkey_patch_print(state.logdir)
+    with monkey_patch_print(state.logdir):
+        for task_class in task_classes:
+            state.tasks.append(task_class(state))
 
-    for task_class in task_classes:
-        state.tasks.append(task_class(state))
+        # Write initial state
+        state.save()
 
-    # Write state
-    state.save()
+        # Start tasks
+        threads = []
+        for task in state.tasks:
+            task_name = task.__class__.__name__
+            # Set thread name to task name for output aggregation
+            thread = threading.Thread(target=task._run, name=task_name)
+            thread.start()
+            threads.append(thread)
+        for thread in threads:
+            thread.join()
 
-    # Start tasks
-    threads = []
-    for task in state.tasks:
-        task_name = task.__class__.__name__
-        # Set thread name to task name for output aggregation
-        thread = threading.Thread(target=task._run, name=task_name)
-        thread.start()
-        threads.append(thread)
-    for thread in threads:
-        thread.join()
+        state.save()
 
 
 def run_all_tasks_in_file(filename: Path, state: State) -> None:
-    monkey_patch_print(state.logdir)
-
-    state.meta["started"] = time.time()
+    # Import tasks file
     try:
-        # Import tasks file
         spec = importlib.util.spec_from_file_location("tasks", filename)
         tasks_module = importlib.util.module_from_spec(spec)
         spec.loader.exec_module(tasks_module)  # type: ignore
         tasks = get_tasks_from_module(tasks_module)
-    except Exception:
-        # Write a file to indicate the run failed
-        state.meta["finished"] = time.time()
+    except (Exception, SyntaxError):
+        tb = traceback.format_exc()
 
         # Dummy task to indicate failure
-        # TODO: Handle this more gracefully
         class FailedImport(Task):
-            def __init__(self, state: State) -> None:
-                super().__init__(state)
-                self.status = Status.failed
-
-        state.tasks.append(FailedImport(state))
-        state.save()
-        raise
-    try:
-        # Run all tasks
-        run_tasks(tasks, state)
-    except Exception:
-        state.meta["finished"] = time.time()
-        state.save()
-        raise
-    state.meta["finished"] = time.time()
-    state.save()
+            def run(self) -> None:
+                print(tb)
+                raise Exception("Import failed")
+        tasks = [FailedImport]
+    # Run all tasks
+    run_tasks(tasks, state)

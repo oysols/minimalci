a36f82cd37a0555fb0349594232592e308fc999c
Øystein Olsen <oystein.olsen@softwarelab.no>
2020-08-26 14:19:29 +0200

    Update self CI

--- tasks.py
+++ tasks.py
@@ -1,55 +1,47 @@
-from pathlib import Path
-
 from minimalci.executors import LocalContainer, Local, Stash
 from minimalci.tasks import Task, Status
+from minimalci import task_util
 
 
 image_name: str
+source: Stash
+
+# github_auth = Path("/secrets/github_auth.txt").read_text().strip()
 
 
 class Setup(Task):
     def run(self) -> None:
         with Local() as exe:
-            global image_name
-            image_name = f"test:{self.state.commit}"
+            global source, image_name
+            source = exe.stash(".")
+            image_name = f"minimalci:{self.state.commit}"
             exe.sh(f"docker build . -t {image_name}")
-            print(self.state.source.read_text("Dockerfile"))
-            exe.unstash(self.state.secrets)
-            #exe.unstash(self.state.secrets, "supersecret.txt")
 
 
 class Test(Task):
     run_after = [Setup]
+
     def run(self) -> None:
         with LocalContainer(image_name, temp_path=True, mount_docker=True) as exe:
-            print(f"Testing commit {self.state.commit}")
-            exe.unstash(self.state.source)
+            exe.unstash(source)
             exe.sh("make test")
 
 
 class Lint(Task):
-    run_after = [Test]
+    run_after = [Setup]
+
     def run(self) -> None:
         with LocalContainer(image_name, temp_path=True) as exe:
-            exe.unstash(self.state.source)
+            exe.unstash(source)
             exe.sh("make check")
 
 
 class ErrorHandler(Task):
     run_after = [Test, Lint]
     run_always = True
+
     def run(self) -> None:
-        for task in self.state.tasks:
-            print(f"{task.name} {task.status.name}")
-
-
-if __name__ == "__main__":
-    import __main__  # type: ignore
-    from minimalci import taskrunner, tasks
-
-    all_tasks = taskrunner.get_tasks_from_module(__main__)
-    with Local() as exe:
-        source = exe.stash("*")
-    state = tasks.State(source, Stash())
-    state.commit = "localtest"
-    taskrunner.run_tasks(all_tasks, state)
+        if all(task.status == Status.success for task in self.state.tasks if task is not self):
+            print("Set github status", task_util.GithubState.success.name)
+        else:
+            print("Set github status", task_util.GithubState.failure.name)
